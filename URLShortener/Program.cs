5bf2fa1deb4c5c42fd15d75ae67cbd86785984eb
Noah-Somers <noahsomers@yahoo.com>
2026-07-14 20:10:45 -0400

    Updated db migration handling.

--- URLShortener/Program.cs
+++ URLShortener/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
